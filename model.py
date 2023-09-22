a37ccbc1d9eb78239765044792d3115ee757caf6
kkt <kkt4828@kakao.com>
2023-09-22 09:12:13 +0900

    updated model.py for transformer

--- model.py
+++ model.py
@@ -2,24 +2,48 @@ import torch
 import torch.nn as nn
 
 class scaledDotProudct(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        qm, km, vm = x
+        qkm = torch.matmul(qm, km.T) / torch.sqrt(qm.shape[0])
+        soft = nn.functional.softmax(torch.matmul(qkm, vm))
+        return soft
+
+class HeadAttetion(nn.Module):
     def __init__(self, embDim, kDim, vDim):
         super().__init__()
-        self.embDim = embDim
         self.q = torch.nn.Parameter(torch.randn((embDim, kDim)))
         self.k = torch.nn.Parameter(torch.randn((embDim, kDim)))
         self.v = torch.nn.Parameter(torch.randn((embDim, vDim)))
+        self.qLinear = nn.Linear(embDim, embDim)
+        self.kLinear = nn.Linear(embDim, embDim)
+        self.vLinear = nn.Linear(embDim, embDim)
+        self.mhaLinear = nn.Linear(vDim, vDim)
 
     def forward(self, x):
-        qm = torch.matmul(x, self.q)
-        km = torch.matmul(x, self.k)
-        vm = torch.matmul(x, self.v)
-        qkm = torch.matmul(qm, km.T) / torch.sqrt(self.q.shape[0])
-        soft = nn.functional.softmax(torch.matmul(qkm, vm))
+        qw = self.qLinear(x)
+        kw = self.kLinear(x)
+        vw = self.vLinear(x)
 
-class multiHeadAttetion(nn.Module):
-    def __init__(self):
-        super().__init__()
+        mhaResult = scaledDotProudct((qw, kw, vw))
+        return mhaResult
+
 
 class feedForward(nn.Module):
-    def __init__(self):
+    def __init__(self, vDim):
         super().__init__()
+        self.linear = nn.Linear(vDim, vDim)
+        self.layerNorm = nn.LayerNorm(vDim)
+
+    def forward(self, x):
+        return self.layerNorm(x + self.linear(x))
+
+class encoder(nn.Module):
+    def __init__(self):
+        pass
+
+class decoder(nn.Module):
+    def __init__(self):
+        pass
